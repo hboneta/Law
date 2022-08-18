1e4fce6f7a63988df36e9885fec304ee7d0b9bc9
hboneta <53717550+hboneta@users.noreply.github.com>
2022-08-17 21:57:52 -0600

    Add files via upload

--- Flashcard.js
+++ Flashcard.js
@@ -1,86 +1,58 @@
 var subjects;
-var terms;
 var termIdx = 0;
 var subjIdx = 0;
 function getSubjects() {
-    fetch('Clanceys1L.json')
+    fetch('Outlines1L.json')
         .then(response => response.json())
         .then(data => {
             subjects = data.subjects;
         });
 }
 
-function renderSubjects() {
-    getSubjects();
-    let html = '';
-    subjects.forEach(function (subj, idx) {
-        html += `<button type="button" onclick="getTerms(${idx})" id="subjBtn${idx}">${subj.subject}</button>`;
-
+function renderSidebar() {
+    var html = '';
+    subjects.forEach((item,idx) => {
+        html += `<div class="w3-bar-item w3-button w3-text-grey w3-hover-black" onclick="closeNav(${idx})">${item.subject}</div>`
     });
-    document.getElementById("maindiv").innerHTML = html;
-}
-function getTerms(subjIdx) {
-    //let subjects = getSubjects();
-    subjIdx = subjIdx;
-    termIdx = 0;
-    terms = subjects[subjIdx].terms;
-    initTerms();
+    document.getElementById("sidenav").innerHTML = html;
 }
 
-function initTerms() {
-    document.getElementById("termdiv").hidden = false;
-    document.getElementById("term").innerHTML = terms[termIdx].term;
-    document.getElementById("definition").hidden = true;
-    if (terms[termIdx].definition) {
-        document.getElementById("definition").innerHTML = terms[termIdx].definition;
-        //document.getElementById("definition").hidden = false;
-        document.getElementById("defBtn").disabled = false;
-    } else {
-        document.getElementById("defBtn").disabled = true;
-    }
-    document.getElementById("keywords").hidden = true;
-    if (terms[termIdx].keywords) {
-        html = '<br>';
-        terms[termIdx].keywords.forEach(item => {
-            html += item + "<br>";
-        });
-        document.getElementById("keyBtn").disabled = false;
-        document.getElementById("keywords").innerHTML = html;
-    } else {
-        document.getElementById("keyBtn").disabled = true;
-    }
+function renderSubject(){
+    var subject=subjects[subjIdx];
+    document.getElementById("subjectdiv").innerHTML= `<h1 id="subject">${subject.subject}</h1>
+            <hr class="w3-opacity">
+            <h2>Mnemonic: </h2><span>${subject.mnemonic}</span>
+            <h3 onclick="toggleMe(document.getElementById('subjectElements'))">Elements</h3>
+            <div id="subjectElements"></div>`
+    var html = '';
+    subject.elements.forEach(item => {
+        html += `<div>${item.element}</div>`
+    });
+    document.getElementById("subjectElements").innerHTML = html;
+    
 
 }
 
-function nextBtnClick() {
-    if (termIdx < terms.length - 1) {
-        termIdx += 1;
-        initTerms();
-    } else {
+// Open and close sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "60%";
+    document.getElementById("mySidebar").style.display = "block";
+    renderSidebar();
+  }
+  
+  function closeNav(inId) {
+    document.getElementById("mySidebar").style.display = "none";
+    subjIdx = inId;
+    renderSubject();
+  }
+  
 
-    }
+function toggleMe (inDoc){
+    inDoc.hidden = !(inDoc.hidden)
 }
 
-function prevBtnClick() {
-    if (termIdx > 0) {
-        termIdx -= 1;
-        initTerms();
-    } else {
-
-    }
-}
-
-function showDef() {
-    document.getElementById("definition").hidden = false;
-}
-
-function showKey() {
-    document.getElementById("keywords").hidden = false;
-}
-
-
-
-//renderSubjects();
+getSubjects();
+//renderSidebar();
 //console.log("test");
-//renderSubjects();
+//getSubjects();
 //getTerms(subjIdx);
